7c03ba578112326063a8f7cafd0325a8685e0612
박동혁 <kaje0god@naver.com>
2021-02-10 12:34:26 +0900

    Fix : index.js  - connect mongoose

--- server/index.js
+++ server/index.js
@@ -3,10 +3,23 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
+import { CONNECTION_URL } from './dbUrl.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+// const CONNECTION_URL = 'FIX'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+mongoose.set('useFindAndModify', false);
 // https://www.mongodb.com/cloud/atlas
