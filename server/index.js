2a9956aafafb06202c25cefe83fcdcb602f8e192
박동혁 <kaje0god@naver.com>
2021-02-26 16:33:43 +0900

    Fix : cors error  - change code sequence

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ import { CONNECTION_URL } from './dbUrl.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 // const CONNECTION_URL = 'FIX'
 const PORT = process.env.PORT || 5000;
