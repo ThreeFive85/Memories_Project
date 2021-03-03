d233d87a3cfc5d405286740908c87d19f104a7ad
박동혁 <kaje0god@naver.com>
2021-03-03 17:28:42 +0900

    Fix : use dispatch  - handleSubmit function

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -13,9 +16,12 @@ const Form = () => {
     })
 
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
