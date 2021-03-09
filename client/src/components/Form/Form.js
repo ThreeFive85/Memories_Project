84cae0f94f5534cab3578e96613588c971ead0af
박동혁 <kaje0god@naver.com>
2021-03-09 17:02:17 +0900

    Fix : useSelector  - useEffect

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
@@ -15,10 +16,14 @@ const Form = ({currentId, setCurrentId}) => {
         message: '',
         tags: '',
         selectedFile: '',
-    })
-
+    });
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null); 
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
