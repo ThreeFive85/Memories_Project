ba378efa4624b2ee53e05c5e04747c80f7a058fd
박동혁 <kaje0god@naver.com>
2021-03-18 17:34:51 +0900

    Fix : post.js  - likePost function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -32,9 +32,9 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatePost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
 
-    res.json(updatePost);
+    res.json(updatedPost);
 }
 
 export const deletePost = async (req, res) => {
@@ -45,4 +45,15 @@ export const deletePost = async (req, res) => {
     await PostMessage.findByIdAndRemove(id);
 
     res.json({ message: 'Post deleted successfully' });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
 }
