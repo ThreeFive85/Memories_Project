17343a9e01076286588a4c09bb96d8ad2574382d
박동혁 <kaje0god@naver.com>
2021-03-23 17:39:57 +0900

    Fix : env  - dotenv

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 import { CONNECTION_URL } from './dbUrl.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -19,7 +21,7 @@ app.use('/posts', postRoutes);
 // const CONNECTION_URL = 'FIX'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 mongoose.set('useFindAndModify', false);
