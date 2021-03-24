58e2062c4675c98155bf294b48d4151344684a02
박동혁 <kaje0god@naver.com>
2021-03-24 13:38:29 +0900

    Fix : dispatch types  - change constant to string

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, DELETE, UPDATE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
@@ -5,7 +6,7 @@ export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
 
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error.message);
     }
@@ -15,7 +16,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data});
+        dispatch({ type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -25,7 +26,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
 
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error.message);
     }
@@ -35,7 +36,7 @@ export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id); // 여기서는 const 변수를 쓸 필요가 없다.(삭제이기때문에 반환데이터는 필요없음)
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
