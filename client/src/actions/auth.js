758fd075a982b02594ff24addc78e289fbb90eec
박동혁 <kaje0god@naver.com>
2021-06-10 12:16:39 +0900

    Feat : use dispatch in signin, signup function

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,6 +4,9 @@ import * as api from '../api';
 export const signin = (formData, history) => async (dispatch) => {
     try {
         // 로그인 유저
+        const { data } = await api.signIn(formData);
+
+        dispatch({ type: AUTH, data });
 
         history.push('/');
     } catch (error) {
@@ -14,6 +17,9 @@ export const signin = (formData, history) => async (dispatch) => {
 export const signup = (formData, history) => async (dispatch) => {
     try {
         // 회원가입 유저
+        const { data } = await api.signUp(formData);
+
+        dispatch({ type: AUTH, data });
 
         history.push('/');
     } catch (error) {
