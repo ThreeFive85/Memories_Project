c89998dd2939cdad6147c55a472455b0b3421736
박동혁 <kaje0god@naver.com>
2021-06-12 14:42:15 +0900

    Fix : postMessage.js, Post.js  - postMessage : add name  - Post.js : post.name

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
+    name: String,
     creator: String,
     tags: [String],
     selectedFile: String, // 이미지를 문자열로 변환(리액트베이스64)
